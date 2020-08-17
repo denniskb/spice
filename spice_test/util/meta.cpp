97aeea0adbb365824f41c9e5af112c1017775e64
dkb <dennis@bautembach.de>
2020-08-17 15:27:06 +0300

    Remove stale code

--- spice_test/util/meta.cpp
+++ spice_test/util/meta.cpp
@@ -40,27 +40,3 @@ TEST( Meta, Map )
 		ASSERT_EQ( std::get<1>( y ), 4.5f );
 	}
 }
-
-/*TEST( Meta, TransformReduce )
-{
-    std::tuple<float, float> x( 1.0f, 2.0f );
-    auto y = x;
-
-    ASSERT_EQ(
-        0,
-        transform_reduce(
-            x,
-            y,
-            0.0f,
-            []( auto x, auto y ) { return std::abs( x - y ); },
-            []( auto x, auto y ) { return x + y; } ) );
-
-    ASSERT_EQ(
-        4.0f,
-        transform_reduce(
-            x,
-            std::tuple<float, float>(),
-            1.0f,
-            []( auto x, auto y ) { return std::abs( x - y ); },
-            []( auto x, auto y ) { return x + y; } ) );
-}*/
