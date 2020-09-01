c885c6912d8f8570c4d290e6fbff6e56da91d2b4
dkb <dennis@bautembach.de>
2020-09-01 18:19:49 +0300

    Fix spice_assert on gpu

--- spice/util/assert.h
+++ spice/util/assert.h
@@ -10,11 +10,14 @@ void _assert( char const * cond, char const * file, int line, char const * msg =
 } // namespace spice
 
 
-// HACK: Disable assert()s in most CUDA code because __CUDA_ARCH__ is borken
-// (gets defined outside device code, anytime? NVCC is running
-#if( defined( SPICE_ASSERT_RELEASE ) || !defined( NDEBUG ) ) && !defined( __CUDA_ARCH__ )
+#if defined( SPICE_ASSERT_RELEASE ) || !defined( NDEBUG )
+#ifndef __CUDA_ARCH__
 #define spice_assert( X, ... ) \
 	(void)( ( X ) || ( ::spice::util::_assert( #X, __FILE__, __LINE__, ##__VA_ARGS__ ), 0 ) )
+#else
+#define spice_assert( X, ... ) \
+	if( !( X ) ) asm( "trap;" );
+#endif
 #else
 #define spice_assert( X, ... )
 #endif
