9195c3895256c462d7acd2f148660119fa032eb9
dkb <dennis@bautembach.de>
2020-11-08 04:23:47 +0200

    Improve bench script

--- bench.sh
+++ bench.sh
@@ -1,88 +1,59 @@
 sims=(/build/release/samples) # bsim ngpu (all sims)
 models=(vogels brunel)
-sizes_single=({200000000..3000000000..200000000})
+sizes_single=({250000000..3000000000..250000000})
 sizes_multi=({500000000..24000000000..500000000})
+gpus=("0" "0,1" "0,1,2,3" "0,1,2,3,4,5,6,7")
+gpu=("single" "multi" "multi" "multi")
+sizes=({250000000..3000000000..250000000}
+{250000000..750000000..250000000} {1000000000..3500000000..500000000} {4000000000..6000000000..1000000000}
+{250000000..750000000..250000000} {1000000000..5000000000..1000000000} {6000000000..12000000000..2000000000}
+{250000000..750000000..250000000} 1000000000 2000000000 4000000000 6000000000 {8000000000..24000000000..4000000000})
 
+sizesbp=({50000000..600000000..50000000}
+{50000000..350000000..50000000} {400000000..1200000000..200000000}
+{50000000..150000000..50000000} {200000000..1000000000..200000000} {1200000000..2400000000..400000000}
+{50000000..150000000..50000000} 200000000 400000000 800000000 1200000000 {1600000000..4800000000..800000000})
 echo "[" > results.json
 
 run() {
 	echo $1
-	eval "$1 >> results.txt"
-	echo -n "," >> results.txt
+	eval "$1 >> results.json"
+	echo -n "," >> results.json
 }
 
 for sim in ${sims[@]}
 do
-	export CUDA_VISIBLE_DEVICES=0
-	
-	# simtime single-gpu
-	for model in vogels brunel
+	for igpu in {0..3}
 	do
-		for size in ${sizes_single[@]}
-		do
-			run ".$sim --bench sim --gpu single --model $model --nsyn $size"
-		done
-	done
-	for size in {50000000..1000000000..50000000}
-	do
-		run ".$sim --bench sim --gpu single --model brunel+ --nsyn $size"
-	done
-	
-	# simtime sparse
-	for size in ${sizes_single[@]}
-	do
-		run ".$sim --bench sim --gpu single --model synth --pconnect 0.00155 --pfire 0.01 --delay 1 --nsyn $size"
-	done
-	
-	# setup time
-	for size in ${sizes_multi[@]}
-	do
-		run ".$sim --bench setup --gpu single --model synth --pconnect 0.05 --pfire 0 --delay 1 --nsyn $size"
-	done
-	
-	unset CUDA_VISIBLE_DEVICES
-	
-	for size in ${sizes_multi[@]}
-	do
-		run ".$sim --bench setup --gpu multi --model synth --pconnect 0.05 --pfire 0 --delay 1 --nsyn $size"
-	done
-	
-	# simtime multi-gpu
-	for gpu in "0,1" "0,1,2,3" "0,1,2,3,4,5,6,7"
-	do
-		export CUDA_VISIBLE_DEVICES=$gpu
-		
+		export CUDA_VISIBLE_DEVICES=${gpus[$igpu]}
+
+		# simtime
 		for model in vogels brunel
 		do
-			for size in ${sizes_multi[@]}
+			for size in ${sizes[@]:$igpu*12:12}
 			do
-				run ".$sim --bench sim --gpu multi --model $model --nsyn $size"
+				run ".$sim --bench sim --gpu ${gpu[$igpu]} --model $model --nsyn $size"
 			done
 		done
-		for size in {100000000..4000000000..100000000}
+		for size in ${sizesbp[@]:$igpu*12:12}
 		do
-			run ".$sim --bench sim --gpu multi --model brunel+ --nsyn $size"
+			run ".$sim --bench sim --gpu ${gpu[$igpu]} --model brunel+ --nsyn $size"
 		done
-	done	
-done
 
-#speedup = our sim x synth model x 3 network sizes x 1..8 delays (8 gpus)
-export CUDA_VISIBLE_DEVICES=0
-for size in 500000000 1000000000 2000000000
-do
-	for delay in {1..8}
+		# setup time
+		for size in ${sizes[@]:$igpu*12:12}
+		do
+			run ".$sim --bench setup --gpu ${gpu[$igpu]} --model synth --pconnect 0.05 --pfire 0 --delay 1 --nsyn $size"
+		done
+	done
+
+	# simtime sparse
+	for size in {250000000..3000000000..250000000}
 	do
-		run ".${sims[0]} --bench sim --gpu single --model synth --pconnect 0.05 --pfire 0.01 --delay $delay --nsyn $size"
+		run ".$sim --bench sim --gpu single --model synth --pconnect 0.0016 --pfire 0.001 --delay 1 --nsyn $size"
 	done
 done
 
-unset CUDA_VISIBLE_DEVICES
-for size in 4000000000 8000000000 16000000000
-do
-	for delay in {1..8}
-	do
-		run ".${sims[0]} --bench sim --gpu multi --model synth --pconnect 0.05 --pfire 0.01 --delay $delay --nsyn $size"
-	done
-done
+# TODO: speedup (as function of delay) (metrics, viz, ..)
 
-echo -n "{}]" >> results.txt
+echo -n "{}]" >> results.json
