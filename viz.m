cc66343252f1438e142375eb38218108dd93ed7b
dkb <dennis@bautembach.de>
2020-12-15 12:21:20 +0200

    Handle steps without spikes in viz

--- viz.m
+++ viz.m
@@ -1,4 +1,5 @@
-f = fopen("~/brunel.txt");
+%f = fopen("~/brunel.txt");
+f = fopen("/media/dkb/data/google_drive/projects/BSim/release/bin/GSim.log");
 n = fgetl(f); n = str2num(n);
 
 width = ceil(sqrt(n));
@@ -6,11 +7,13 @@ i = zeros(width * width, 1);
 
 l = fgetl(f);
 while ischar(l)
-    spikes = split(l, ',');
-    spikes = uint32(arrayfun(@(x) str2num(x{1}), spikes)) + 1;
-    
     i(:) = 0;
-    i(spikes) = 1;
+    
+    if length(l) > 0
+        spikes = split(l, ',');
+        spikes = uint32(arrayfun(@(x) str2num(x{1}), spikes)) + 1;
+        i(spikes) = 1;
+    end
     
     imshow(reshape(i, width, width)', 'InitialMagnification', 400);
     
