e5b083b96848d99b7d738b0739db07cdd877bc4e
dkb <dennis@bautembach.de>
2021-03-28 00:49:39 +0200

    Restore viz

--- results/viz.m
+++ results/viz.m
@@ -2,22 +2,22 @@ clc;
 close all;
 
 %f = fopen("~/brunel.txt");
-f = fopen("/media/dkb/data/google_drive/projects/BSim/release/GSim.log");
+f = fopen("/media/dkb/data/google_drive/projects/ijcnn2020/build/release/test.txt");
 n = fgetl(f); n = str2num(n);
 
 width = ceil(sqrt(n));
 i = zeros(width * width, 1);
 
 l = fgetl(f);
-avg = [];
+total = 0;
 while ischar(l)
     i(:) = 0;
     
     if length(l) > 0
         spikes = split(l, ',');
-        spikes = uint32(str2double(spikes)) + 1;
+        spikes = str2double(spikes) + 1;
         i(spikes) = 1;
-        avg = [avg; length(spikes)];
+        total = total + length(spikes);
     end
     
     imshow(reshape(i, width, width)', 'InitialMagnification', 400);
@@ -25,4 +25,4 @@ while ischar(l)
     l = fgetl(f);
 end
 
-sum(avg)/n/1000
+total/n/1000
